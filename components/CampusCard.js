bdc7a47ed33081388b3dbfcd01fb5e026ab1dadd
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-17 16:05:06 +0200

    leen-eindopdracht- stylen campusCard zonder dynamische styling van color

--- components/CampusCard.js
+++ components/CampusCard.js
@@ -2,13 +2,15 @@ import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const CampusCard = ({}) => {
+const CampusCard = ({ color }) => {
   const navigation = useNavigation();
   return (
-    <View>
-      <Text style={styles.cardHeader}>Campus naam</Text>
+    <View style={styles.cardContainer}>
+      <View style={styles.cardHeaderContainer}>
+        <Text style={styles.cardHeader}>Campus naam</Text>
+        <Text style={styles.cardFocus}>Campus focus</Text>
+      </View>
       <Text style={styles.cardAdres}>Campus adres</Text>
-      <Text style={styles.cardFocus}>Campus focus</Text>
       <TouchableOpacity>
         <Text style={styles.cardButton}>Bekijk campus aanbod</Text>
       </TouchableOpacity>
@@ -16,6 +18,57 @@ const CampusCard = ({}) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardContainer: {
+    backgroundColor: "#979797",
+    margin: 20,
+    padding: 16,
+    borderRadius: 16,
+    shadowColor: "#000000a8",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 8,
+  },
+  cardHeaderContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  cardHeader: {
+    fontSize: 28,
+    fontFamily: "Poppins",
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#fff",
+  },
+  cardFocus: {
+    fontSize: 14,
+    fontFamily: "Arial",
+    backgroundColor: "#fff",
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 8,
+  },
+  cardAdres: {
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: "Arial",
+    marginBottom: 10,
+  },
+  cardButton: {
+    fontSize: 16,
+    fontFamily: "Arial",
+    backgroundColor: "#fff",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 8,
+    marginTop: 10,
+    alignSelf: "flex-start",
+  },
+});
 
 export default CampusCard;
