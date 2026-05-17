ab8c40b17dd2c9c53dda1aae206dadf79b47271d
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-17 14:29:27 +0200

    leen-eindopdracht- setup simpel homescreen om components te testen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+const HomeScreen = ({ navigation }) => {
+  return <ScrollView></ScrollView>;
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
