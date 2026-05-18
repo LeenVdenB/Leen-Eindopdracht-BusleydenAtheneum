f1f3f4968293013b754bc43e6f40b825315605b2
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-18 13:52:20 +0200

    leen-eindopdracht- refactor App.js om TabNavigator te gebruiken voor navigatie

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./screens/HomeScreen";
-import CampusDetailScreen from "./screens/CampusDetailScreen";
-import CustomHeader from "./components/CustomHeader";
-
-const Stack = createNativeStackNavigator();
+import TabNavigator from "./navigation/TabNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            header: () => <CustomHeader title="Busleyden Atheneum" />,
-          }}
-        />
-        <Stack.Screen
-          name="CampusDetail"
-          component={CampusDetailScreen}
-          options={{
-            header: () => <CustomHeader title="Campus Details" />,
-          }}
-        />
-      </Stack.Navigator>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
