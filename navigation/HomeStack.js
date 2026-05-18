9ebc0fe57055b5696bfec6b90d5bb0374c5759ad
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-18 13:53:43 +0200

    leen-eindopdracht- fix import andere screens (.) ipv (..)

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,33 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from "./screens/HomeScreen";
-import CampusDetailScreen from "./screens/CampusDetailScreen";
-import CustomHeader from "./components/CustomHeader";
+import HomeScreen from "../screens/HomeScreen";
+import CampusDetailScreen from "../screens/CampusDetailScreen";
+import CustomHeader from "../components/CustomHeader";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+export default function HomeStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            header: () => <CustomHeader title="Busleyden Atheneum" />,
-          }}
-        />
-        <Stack.Screen
-          name="CampusDetail"
-          component={CampusDetailScreen}
-          options={{
-            header: () => <CustomHeader title="Campus Details" />,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          header: () => <CustomHeader title="Busleyden Atheneum" />,
+        }}
+      />
+      <Stack.Screen
+        name="CampusDetail"
+        component={CampusDetailScreen}
+        options={{
+          header: () => <CustomHeader title="Campus Details" />,
+        }}
+      />
+    </Stack.Navigator>
   );
 }
