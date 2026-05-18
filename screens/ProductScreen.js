40de5181722e2ba6e9b9f23f60d88c943ca77f39
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-18 18:37:06 +0200

    leen-eindopdracht- Parameters meegeven aan product screen zodat het de juiste info toont bij detailscherm en card

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,14 +1,61 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import { useNavigation } from "@react-navigation/native";
 
+const categoryNames = {
+  "6a06d8853930dd58a0e94469": "Accessoires",
+  "6a06d87ca873ba1cdbba176c": "Baby & kids",
+  "6a06d868bb279e3f7fcd4614": "School spullen",
+  "6a06d801e250df66b10ff46c": "Sweater",
+  "6a06d7faef120ae92b377b6a": "Hoodie",
+};
+
 const ProductScreen = ({ navigation }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.webflow.com/v2/sites/6a046dc62c1f6537649e8068/products",
+      {
+        headers: {
+          authorization:
+            "Bearer 4970d0a9d6a3f5c38c7d2fee3f0a717763c879474c73910e9d9baa15ff0b6cf2",
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(
+          data.items.map((item) => ({
+            id: item.product.id,
+            title: item.product.fieldData.name,
+            description: item.product.fieldData.description,
+            price: (item.skus[0]?.fieldData.price.value || 0) / 100,
+            image: { uri: item.skus[0]?.fieldData["main-image"]?.url },
+            category:
+              categoryNames[item.product.fieldData.category[0]] ||
+              "Onbekende categorie",
+          })),
+        );
+      })
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Producten</Text>
-      <ProductCard />
-    </View>
+    <ScrollView>
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.title}
+          description={product.description}
+          price={product.price}
+          image={product.image}
+          category={product.category}
+          onPress={() => navigation.navigate("ProductDetail", { ...product })}
+        />
+      ))}
+    </ScrollView>
   );
 };
 
