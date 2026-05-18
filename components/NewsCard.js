3e73d15bf508132072bbc17e7b2558e7866c452b
Leen Van den Bossche <r1060160@student.thomasmore.be>
2026-05-18 15:29:43 +0200

    leen-eindopdracht- NewsCard component uitwerken zonder parameters of props

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,11 +1,28 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const NewsCard = ({}) => {
   const navigation = useNavigation();
   return (
     <View style={styles.card}>
-      <Text style={styles.title}>News Title</Text>
+      <Text style={styles.header}>News Title</Text>
+      <Image
+        source={{ uri: "https://via.placeholder.com/300" }}
+        style={styles.image}
+      />
+      <Text style={styles.category}>Category</Text>
       <Text style={styles.summary}>This is a summary of the news article.</Text>
+      <Text style={styles.date}>Date</Text>
+      <TouchableOpacity>
+        <Text style={styles.button}>Lees meer</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -17,13 +34,48 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 10,
   },
-  title: {
-    fontSize: 18,
+  header: {
+    fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 8,
   },
   summary: {
     marginTop: 6,
+    fontSize: 16,
+    color: "#000000",
+  },
+  category: {
+    borderColor: "#86BC25",
+    borderWidth: 1,
+    alignSelf: "flex-start",
+    padding: 8,
+    borderRadius: 8,
+    fontSize: 12,
+    color: "#000000",
+    marginBottom: 4,
+  },
+  date: {
+    fontSize: 12,
+    color: "#000000",
+    marginTop: 4,
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: "#86BC25",
+    color: "#fff",
     fontSize: 14,
-    color: "#555",
+    fontWeight: "bold",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 8,
+    alignSelf: "flex-start",
+  },
+  image: {
+    width: "100%",
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
   },
 });
+
+export default NewsCard;
